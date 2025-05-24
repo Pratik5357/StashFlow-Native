036d47ecba82906374c75b485ede5155d95fce9f
Pratik5357 <pratikkumbhar008@gmail.com>
2025-05-24 12:25:34 +0530

    1.reseted project for new app 2.Installed native wind 3.Used tailwind classes to ensure its working

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-5xl">Welcome to StashFlow</Text>
     </View>
   );
 }
